74406397b756fe47879c93c590867138e03ee6a7
samplb <a01401504@unet.univie.ac.at>
2018-01-15 18:55:00 +0100

    php-Directory unterteilung

---
+++ WebContent/mongoDBphpFiles/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>AMSTEC Management</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<frameset>
+    <frame>
+    <frame>
+    <noframes>
+    <body>
+    <p>index page, ideal wären links zu untermenüs, also pro use-case eine eigene php file</p>
+    <?php
+
+	?>
+    </body>
+    </noframes>
+</frameset>
+</html>
